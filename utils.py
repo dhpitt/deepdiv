f1e8ac863d02e7d2eafe1d99d9a0e8402c770ae8
dhpitt <david.h.pitt@gmail.com>
2022-12-08 13:26:20 -0800

    everything appears to work

--- utils.py
+++ utils.py
@@ -19,16 +19,23 @@ def HSIC(K: torch.tensor, L: torch.tensor):
     # independence criterion. K and L are matrices 
     assert(K.size()[0] == L.size()[0], "Error: K and L do not have the same number of examples.")
     n = K.size()[0]
-    H_n = centering_matrix(size=n)
+    H_n = centering_matrix(size=n).cuda()
     product = K @ H_n @ L @ H_n
 
     return 1/((n - 1)**2) * torch.trace(product)
 
-def CKA(K: torch.tensor, L: torch.tensor):
+def CKA(X: torch.tensor, Y: torch.tensor, kernel='linear'):
     # Returns the Centered Kernel Alignment similarity metric
     # of feature matrices K, L (assumed to be passed through kernels)
-    assert(K.size()[0] == L.size()[0], "Error: K and L do not have the same number of examples.")
+    assert(X.size()[0] == Y.size()[0], "Error: X1 and X2 do not have the same number of examples.")
 
+    # kernel: one str of {linear, rbf}
+
+    if kernel == 'rbf':
+        raise NotImplementedError()
+    elif kernel == 'linear': # dot-product similarity of all examples 
+        K = X @ X.T
+        L = Y @ Y.T 
     KL = HSIC(K, L)
     KK = HSIC(K,K)
     LL = HSIC(L,L)
